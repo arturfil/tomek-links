8719cf08b515776acb8a70d3fa59043ebc5fb376
arturfil <arturfil@hotmail.com>
2020-09-10 02:50:35 -0400

    setup for importing data and start the train data set and the test data set

--- data.py
+++ data.py
@@ -1,18 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-# Import Data Set
-dataset = pd.read_csv('Social_Network_Ads.csv')
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:, -1].values
+url = "https://archive.ics.uci/edu/ml/machine-learning-databases/iris/iris.data"
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+# Assign column names to the dataset
+names = ['sepal-lenght', 'sepal-width', 'petal-lenght', 'petal-width', 'Class']
 
-print()
-
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=5, metric = 'minkowski', p = 2)
-classifier.fit(x_train, y_train)
+# Read dataset
+df = pd.read_csv(url, names=names)
